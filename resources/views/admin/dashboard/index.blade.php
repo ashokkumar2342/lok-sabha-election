d3a8c5ad3e7cfbbf6dc84cd84ca4f7e6032f2528
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-04-05 20:00:20 +0530

    dashboead link home

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -10,7 +10,10 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              @if (Auth::user()->role==2)
+              
+              <li class="breadcrumb-item"><a href="{{ route('home') }}">Refresh</a></li>
+              @endif
                
             </ol>
           </div>
--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -9,7 +9,7 @@
               <!-- Add icons to the links using the .nav-icon class
                    with font-awesome or any other icon font library -->
               <li class="nav-item has-treeview ">
-                <a href="#" class="nav-link">
+                <a href="{{ route('home') }}" class="nav-link">
                   <i class="nav-icon fa fa-dashboard"></i>
                   <p>
                     Dashboard
